928fd4e2e929a54bd9b05673377c470611f66161
Caleb Frey <caleb.frey@rutgers.edu>
2018-07-18 16:39:11 -0400

    Made Portable
    Used the %cd% variable instead of hardcoding the paths

--- Audio Swapper.bat
+++ Audio Swapper.bat
@@ -1,10 +1,10 @@
 
-if exist "C:\Users\Jakob Coughlan\Desktop\speaker.txt" (
-del "C:\Users\Jakob Coughlan\Desktop\speaker.txt"
+if exist "%cd%\speaker.txt" (
+del "%cd%\speaker.txt"
 nircmd setdefaultsounddevice "Headset" 1
 exit
 ) 
 else (
-  echo.>"C:\Users\Jakob Coughlan\Desktop\speaker.txt"
+  echo.>"%cd%\speaker.txt"
   nircmd setdefaultsounddevice "Speakers" 1
 )
